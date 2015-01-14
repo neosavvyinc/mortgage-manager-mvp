c03ecc26e8a03f34c41442e37e335399c67dd124
pabloalonso <35wn793@00f>
2015-01-14 18:39:02 -0500

    removed mocks

--- client/src/main/js/models/model-user.js
+++ client/src/main/js/models/model-user.js
@@ -9,57 +9,50 @@ function User () { }
 User.login = function (email, password){
 
     return Q.promise(function(resolve, reject){
-        resolve({email:"pablo@gmail.com"});
-    //    $.post(Endpoints.LOGIN, {
-    //        email: email,
-    //        password: password
-    //    }).success(function(response){
-    //        resolve(response);
-    //    }).error(function(error){
-    //        reject(error);
-    //    });
+        $.post(Endpoints.LOGIN, {
+            email: email,
+            password: password
+        }).success(function(response){
+            resolve(response);
+        }).error(function(error){
+            reject(error);
+        });
     });
 };
 
 User.register = function (newUser){
     return Q.promise(function(resolve, reject){
-        resolve({userId: "123"});
-
-    //    $.post(Endpoints.REGISTER, newUser)
-    //        .success(function(response){
-    //            resolve(response);
-    //        })
-    //        .error(function(error){
-    //            reject(error);
-    //        });
+        $.post(Endpoints.REGISTER, newUser)
+            .success(function(response){
+                resolve(response);
+            })
+            .error(function(error){
+                reject(error);
+            });
     });
 };
 
 User.update = function (userInfo){
-
     return Q.promise(function(resolve, reject){
-        resolve();
-    //    $.post(Endpoints.USER.ONE.URL.replace(':id', userInfo.id), userInfo)
-    //        .success(function(response){
-    //            resolve(response);
-    //        })
-    //        .error(function(error){
-    //            reject(error);
-    //        });
+        $.post(Endpoints.USER.ONE.URL.replace(':id', userInfo.id), userInfo)
+            .success(function(response){
+                resolve(response);
+            })
+            .error(function(error){
+                reject(error);
+            });
     });
 };
 
 User.addCoapplicant = function (applicantID, coapplicantInfo){
-
     return Q.promise(function(resolve, reject){
-        resolve({email: "pablo@gmail.com"});
-        //    $.post(Endpoints.USER.ONE.COAPPLICANT.URL.replace(':id', applicantID), coapplicantInfo)
-        //        .success(function(response){
-        //            resolve(response);
-        //        })
-        //        .error(function(error){
-        //            reject(error);
-        //        });
+            $.post(Endpoints.USER.ONE.COAPPLICANT.URL.replace(':id', applicantID), coapplicantInfo)
+                .success(function(response){
+                    resolve(response);
+                })
+                .error(function(error){
+                    reject(error);
+                });
     });
 };
 
