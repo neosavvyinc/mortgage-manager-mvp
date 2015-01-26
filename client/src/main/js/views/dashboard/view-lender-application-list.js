65b3f26313953817b949ad03902934c0fa36d3c5
pabloalonso <35wn793@00f>
2015-01-26 14:41:06 -0500

    server endpoint for both lenders and borrowers

---
+++ client/src/main/js/views/dashboard/view-lender-application-list.js
@@ -0,0 +1,91 @@
+var React = require('react');
+var Router = require('react-router');
+var Reflux = require('reflux');
+var _ = require('lodash');
+var moment = require('moment');
+
+var User = require('../../models/model-user');
+var ErrorMessage = require('../../components/error-message');
+var UserStore = require('../../stores/store-user');
+var ApplicationStore = require('../../stores/store-application');
+var ApplicationActions = require('../../actions/action-application');
+
+var Applications = React.createClass({
+
+    mixins: [
+        Router.State,
+        Router.Navigation,
+        Reflux.listenTo(ApplicationStore, 'onApplicationTransition')
+    ],
+
+    getInitialState: function(){
+        return {
+            applications: []
+        }
+    },
+
+    componentDidMount: function(){
+        User.getApplications(UserStore.getCurrentUserId()).then(function(applications) {
+            if (this.isMounted()) {
+                this.setState({
+                    applications: applications
+                });
+            }
+        }.bind(this));
+    },
+
+    render: function(){
+
+        var applicationsTable = [];
+
+        _.map(this.state.applications, function(app){
+            // e.g. Wednesday, January 21, 2015 3:21 PM
+            app.lastUpdated = moment(app.lastUpdated).format('llll');
+            applicationsTable.push((
+                <tr>
+                    <th>{app.primary_applicant}</th>
+                    <th>{app.coapplicant || "None"}</th>
+                    <th>{app.lastUpdated}</th>
+                    <th>
+                        <div className="row">
+                            <button className="btn turquoise one half" onClick={this.onApplicationSelect.bind(null, app)}>View</button>
+                            <button className="btn red one half">Delete</button>
+                        </div>
+                    </th>
+                </tr>
+            ));
+        }, this);
+        return (
+            <div className="container">
+                <div className="gap-top">
+                    <h1>Applications</h1>
+                    <table className="responsive">
+                        <thead>
+                            <tr>
+                                <th>Primary Applicant</th>
+                                <th>Co-Applicant</th>
+                                <th>Last Updated</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        {applicationsTable.map(function(application) {
+                            return (application);
+                        })}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        );
+    },
+
+    onApplicationSelect: function(application) {
+        //ApplicationActions.selectApplication(application);
+    },
+
+    onApplicationTransition: function(){
+        //this.transitionTo('dashboardDocuments', {appId: ApplicationStore.getCurrentApplication()._id});
+    }
+});
+
+module.exports = Applications;
--- server/lib/services/service-application.js
+++ server/lib/services/service-application.js
@@ -4,16 +4,127 @@ var async = require('async');
 var _ = require('underscore');
 var applicationModel = require('../db/models/model-application').Model;
 var userDetailsModel = require('../db/models/model-user-details').Model;
+var userModel = require('../db/models/model-user').Model;
 var documentModel = require('../db/models/model-document').Model;
+var applicationLendersModel = require('../db/models/model-application-lenders').Model;
 var applicationService = require('./service-application');
 var documentService = require('./service-document');
 
-exports.getUserApplications = function(uid, callback){
+exports.getUserApplications = function(uid, success, failure){
     var application = new applicationModel();
-    application.retrieve({pUID: uid}, function(apps){
-        callback(apps);
-    }, function(error){
-        callback(null, error);
+    var applicationLenders = new applicationLendersModel();
+    var userDetails = new userDetailsModel();
+    var user = new userModel();
+
+    var userType,
+        applications = [];
+
+    async.series([
+        /*
+            Get type of the user requesting all applications
+         */
+        function(done){
+            user.retrieve({_id: uid}, function(userData){
+                userType = userData[0].toObject().type;
+                done();
+            }, function(error){
+                done(error);
+            });
+        },
+        /*
+            If the user is a borrower, retrieve all user's applications
+            If the user is a lender, search for all the appIDs related to that lender in the ApplicationLender Table
+         */
+        function(done){
+            if(userType === 'borrower'){
+                application.retrieve({pUID: uid}, function(apps){
+                    applications = apps;
+                    done();
+                }, function(error){
+                    done(error);
+                });
+            } else if (userType === 'lender'){
+                applicationLenders.retrieve({lenderId: uid}, function(applicationLenderData){
+                    async.each(applicationLenderData, function(appLenderData, callback){
+                        application.retrieve({_id: appLenderData.appId}, function(app){
+                            applications.push(app[0]);
+                            callback();
+                        }, callback);
+                    }, function(error){
+                        if(error){
+                            done(error);
+                        } else {
+                            done();
+                        }
+                    });
+                });
+            } else {
+                done(new Error('Internal Server Error'));
+            }
+        },
+        /*
+            If the user is a lender, load the user Details for the primary applicant and the coapplicant of each application related to that lender
+         */
+        function(done){
+            if( userType === 'lender') {
+                async.each(applications,
+                    function (app, callback) {
+                        async.parallel([
+                                /*
+                                    Get all primary user details
+                                 */
+                                function (cb) {
+                                    userDetails.retrieve({_id: app.pUID}, function (userData) {
+                                        _.extend(app, {
+                                            primaryFirstName: userData[0].firstName,
+                                            primaryLastName: userData[0].lastName
+                                        });
+                                        cb();
+                                    }, cb);
+                                },
+                                /*
+                                    Get all coapplicant user details if there are any
+                                 */
+                                function (cb) {
+                                    if (app.coUID) {
+                                        userDetails.retrieve({_id: app.coUID}, function (userData) {
+                                            _.extend(app, {
+                                                coappFirstName: userData[0].firstName,
+                                                coappLastName: userData[0].lastName
+                                            });
+                                            cb();
+                                        }, cb);
+                                    } else {
+                                        cb();
+                                    }
+                                }
+                            ],
+                            function (error) {
+                                if (error) {
+                                    callback(error);
+                                } else {
+                                    callback();
+                                }
+                            });
+                    },
+                    function (error) {
+                        if (error) {
+                            done(error);
+                        } else {
+                            done();
+                        }
+                    }
+                );
+            } else {
+                done();
+            }
+        }
+    ], function (error){
+        if(error){
+            failure(error);
+        } else {
+            success(applications);
+        }
     });
 };
 
