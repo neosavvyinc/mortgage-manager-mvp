3c5a162804a89334f222daeb0694fec639fac1a9
Adam Parrish <aparrish@neosavvy.com>
2015-02-01 16:23:16 -0500

    npm install! argh.

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 # Generate the server side build archive
-cd server ; grunt build ; cd ..
+cd server ; npm install;  grunt build ; cd ..
 cp server/MAM-server.tgz ansible/roles/deploy/files/
 
 # Generate the client side build archive
-cd client ; grunt deploy ; cd ..
-cp client/MAM-client.zip ansible/roles/deploy/files/
+cd client ; npm install; grunt deploy ; cd .. ;cp client/MAM-client.zip ansible/roles/deploy/files/
 
 # Run ansible playbook
 ansible-playbook -i ansible/hosts/staging ansible/staging.yml
