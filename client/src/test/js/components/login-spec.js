b4a92bdf789b069c4457f1c43045da56097ef49a
Sushi Harikrishnan <sushi@neosavvy.com>
2015-02-09 10:33:33 -0500

    Added a couple more tests.

--- client/src/test/js/components/login-spec.js
+++ client/src/test/js/components/login-spec.js
@@ -1,4 +1,5 @@
 jest.dontMock('../../../main/js/components/login');
+jest.dontMock('../../../main/js/components/message-box');
 
 var React = require('react/addons');
 var func = React.PropTypes.func;
@@ -6,78 +7,126 @@ var TestUtils = React.addons.TestUtils;
 var User = require('../../../main/js/models/model-user');
 var Router = require('react-router');
 
-var Login = require('../../../main/js/components/login');
+var LoginPath = '../../../main/js/components/login';
 
-var TestWrapper = React.createClass({
-  childContextTypes: {
-    makePath: func,
-    makeHref: func,
-    transitionTo: func,
-    replaceWith: func,
-    goBack: func,
-    getCurrentPath: func,
-    getCurrentRoutes: func,
-    getCurrentPathname: func,
-    getCurrentParams: func,
-    getCurrentQuery: func,
-    isActive: func,
-  },
+var _createTestWrapper = function(path) {
+	var Component = require(path);
 
-  getChildContext: function () {
-    return {
-      makePath: function () {},
-      makeHref : function () {},
-      transitionTo: function  () {},
-      replaceWith: function  () {},
-      goBack: function  () {},
-      getCurrentPath: function  () {},
-      getCurrentRoutes: function  () {},
-      getCurrentPathname: function  () {},
-      getCurrentParams: function  () {},
-      getCurrentQuery: function  () {},
-      isActive: function  () {},
-    };
-  },
+	return TestWrapper = React.createClass({
+		childContextTypes: {
+			makePath: func,
+			makeHref: func,
+			transitionTo: func,
+			replaceWith: func,
+			goBack: func,
+			getCurrentPath: func,
+			getCurrentRoutes: func,
+			getCurrentPathname: func,
+			getCurrentParams: func,
+			getCurrentQuery: func,
+			isActive: func,
+		},
 
-  render: function () {
-    return (<Login />);
-  }
-});
+		getChildContext: function () {
+			return {
+				makePath: function () {
+				},
+				makeHref: function () {
+				},
+				transitionTo: function () {
+				},
+				replaceWith: function () {
+				},
+				goBack: function () {
+				},
+				getCurrentPath: function () {
+				},
+				getCurrentRoutes: function () {
+				},
+				getCurrentPathname: function () {
+				},
+				getCurrentParams: function () {
+				},
+				getCurrentQuery: function () {
+				},
+				isActive: function () {
+				},
+			};
+		},
+
+		render: function () {
+			return (<Component />);
+		}
+	});
+};
 
 describe('Login', function () {
-    var loginForm,
-        formInstance;
+	var loginForm,
+		button,
+		inputFields,
+		messageBox,
+		email,
+		password,
+		LoginWrapper = _createTestWrapper(LoginPath);
 
-    beforeEach (function () {
+	beforeEach (function () {
+		spyOn(User, 'login').andReturn({
+			then: function () { }
+		});
 
-        spyOn(User, 'login').andReturn({
-            then: function () { }
-        });
+		loginForm = TestUtils.renderIntoDocument(<LoginWrapper />);
+		button = TestUtils.findRenderedDOMComponentWithTag(loginForm, 'button');
+		inputFields = TestUtils.scryRenderedDOMComponentsWithTag(loginForm, 'input');
+		messageBox = TestUtils.scryRenderedDOMComponentsWithClass(loginForm, 'error');
+		email = inputFields[0];
+		password = inputFields[1];
+	});
 
-        loginForm = TestUtils.renderIntoDocument(<TestWrapper />);
-        formInstance = loginForm.getDOMNode();
-    });
+	afterEach(function() {
+		if(loginForm && loginForm.isMounted()) {
+			React.unmountComponentAtNode(loginForm.getDOMNode());
+		}
+	});
 
-    // TODO probably should NOT call this if the fields are empty
-    it("should call User.login", function(){
-        var button = TestUtils.findRenderedDOMComponentWithTag(loginForm, 'button');
-        TestUtils.Simulate.click(button);
+	it('should call User.login', function() {
+		TestUtils.Simulate.click(button);
+		expect(User.login).toHaveBeenCalled();
+	});
 
-        expect(User.login).toHaveBeenCalled();
-    });
+	it('should not call User.login if email field is empty', function() {
+		email.getDOMNode().value = '';
+		password.getDOMNode().value = '*****';
 
-    it("should submit email and password to User.login", function(){
-        var button = TestUtils.findRenderedDOMComponentWithTag(loginForm, 'button');
-        var inputFields = TestUtils.scryRenderedDOMComponentsWithTag(loginForm, 'input');
-        var emailInput = inputFields[0];
-        var passwordInput = inputFields[1];
+		TestUtils.Simulate.click(button);
+		expect(messageBox[0].props.className).toBe('message gap-bottom error hidden');
+		expect(messageBox[0].props.children).toBe('There was an error with your credentials. Please try again.');
+	});
 
-        emailInput.getDOMNode().value = 'contact@neosavvy.com';
-        passwordInput.getDOMNode().value = 'knowledgeIsModern';
-        TestUtils.Simulate.click(button);
+	it('should not call User.login if password field is empty', function() {
+		email.getDOMNode().value = 'contact@neosavvy.com';
+		password.getDOMNode().value = '';
 
-        expect(User.login).toHaveBeenCalledWith('contact@neosavvy.com', 'knowledgeIsModern');
-    });
+		TestUtils.Simulate.click(button);
+		expect(messageBox[0].props.className).toBe('message gap-bottom error hidden');
+		expect(messageBox[0].props.children).toBe('There was an error with your credentials. Please try again.');
+	});
+
+	it('should not call User.login if any of the fields are empty', function() {
+		email.getDOMNode().value = '';
+		password.getDOMNode().value = '';
+
+		TestUtils.Simulate.click(button);
+		expect(messageBox[0].props.className).toBe('message gap-bottom error hidden');
+		expect(messageBox[0].props.children).toBe('There was an error with your credentials. Please try again.');
+	});
+
+	it('should submit email and password to User.login', function() {
+		email.getDOMNode().value = 'contact@neosavvy.com';
+		password.getDOMNode().value = 'knowledgeIsModern';
+		TestUtils.Simulate.click(button);
+
+		expect(User.login).toHaveBeenCalledWith('contact@neosavvy.com', 'knowledgeIsModern');
+	});
 });
 
 
