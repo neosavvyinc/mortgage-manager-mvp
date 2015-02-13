46825d3cc8ceea59968047f1cea609daec4eac9f
Sushi Harikrishnan <sushi@neosavvy.com>
2015-02-13 15:38:53 -0500

    Coapp check for insertNewAPp

--- server/lib/db/models/model-application.js
+++ server/lib/db/models/model-application.js
@@ -65,12 +65,16 @@ applicationModel.insertNewApp = function(applicantDetails,
 			userDetails.update(applicantDetails, {_id: applicantDetails._id}, null, done, done);
 		},
 		function(done) {
-			//Update the coApplicant details with the new appId
-			coapplicantDetails.appId.push(appId);
-			if(coapplicantDetails.toObject !== undefined ) {
-				coapplicantDetails = coapplicantDetails.toObject();
+			if(coapplicantDetails) {
+				//Update the coApplicant details with the new appId
+				coapplicantDetails.appId.push(appId);
+				if (coapplicantDetails.toObject !== undefined) {
+					coapplicantDetails = coapplicantDetails.toObject();
+				}
+				userDetails.update(coapplicantDetails, {_id: coapplicantDetails._id}, null, done, done);
+			} else {
+				done();
 			}
-			userDetails.update(coapplicantDetails, {_id: coapplicantDetails._id}, null, done, done);
 		}
 	], function(error) {
 		if(error !== undefined) {
