27c5fe09eacb9fae8a0dbb4ff9b26fb3c558b14d
pabloalonso <35wn793@00f>
2015-02-19 16:19:54 -0500

    spinner replaced

--- client/src/main/js/views/dashboard/view-upload-document.js
+++ client/src/main/js/views/dashboard/view-upload-document.js
@@ -124,7 +124,7 @@ var UploadDocument = React.createClass({
 	},
 
 	render: function() {
-		return (this.state.loader) ? (<div className="loader"> <img src="http://www.kingpizza.com.br/imagens/loader.gif"/> </div>) :
+		return (this.state.loader) ? (<div className="loader"> <i className="fa fa-spinner fa-pulse"></i> </div>) :
 			(
 				<div>
 					<form className="uploadComponent" encType="multipart/form-data">
