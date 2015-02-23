0f27d90ee48606e48c37efa36a0abe7d53037b52
pabloalonso <35wn793@00f>
2015-02-23 14:54:21 -0500

    modal classes abstraction

--- client/src/main/js/components/modal.js
+++ client/src/main/js/components/modal.js
@@ -9,9 +9,13 @@ var Modal = React.createClass({
 	render: function() {
 		return (
 			<div className="modalDialog">
-				<div className="row">
-					<RouterHandler/>
-				</div>
+                <div className="modal" style={{display: 'block'}}>
+                    <div className="modal-dialog">
+                        <div className="modal-content">
+                            <RouterHandler/>
+                        </div>
+                    </div>
+                </div>
 			</div>
 		);
 	}
